ca5bd98f1e4e30d852435c59420d6b02d1bf68c6
afoxball <55834316+afoxball@users.noreply.github.com>
2024-04-06 16:27:48 +0000

    Beautified imports

--- basic.py
+++ basic.py
@@ -8,7 +8,8 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 from werkzeug.security import generate_password_hash, check_password_hash
-import cairo, random, csv
+import cairo
+import random
 from fpdf import FPDF
 from email_validator import validate_email, EmailNotValidError
 from datetime import datetime
